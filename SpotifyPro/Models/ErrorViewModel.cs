7e1ff2e288306935848ed4e82ace4b89a7b22f28
neydc <neydc.22@gmail.com>
2020-05-24 20:52:38 -0500

    first commit

---
+++ SpotifyPro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpotifyPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
